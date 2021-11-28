94d6906b07fec820fb5213fd0cc139158e094b66
Tjandra <tjandraputra2000@gmail.com>
2021-11-28 15:56:01 +0800

    fixed: Register validation algorithm new

--- back-end/anico_application/forms.py
+++ back-end/anico_application/forms.py
@@ -21,57 +21,77 @@ def validate_register_form(req):
             error_msg_dict[err] = "Field is empty."
 
     for field, value in req.items():
-        # validate email field
-        if field == 'email':
-            email = value
 
-            # check email format
-            if(re.fullmatch(regex, email) == None):
-                error_msg_dict[field] = "Email is invalid."
+        # valid email
+        if error_msg_dict['email'] == '': # value of email field is not empty
 
-            # # check if email exist in database
-            # user_data = User.query.filter_by(email=email).first()
+            # Initiation
+            user_data = User.query.filter_by(email=req['email']).first() # By email address
 
-            # if user_data:
-            #     error_msg_dict[field] = "This email is already taken."
+            # validate email field
+            if field == 'email':
+                email = value
 
-        # validate phone field
-        if field == 'phone':
-            phone = value
-            numerals = "0123456789+"
+                # check email format
+                if(re.fullmatch(regex, email) == None):
+                    error_msg_dict[field] = "Email is invalid."
 
-            # check if email exist in database
-            # user_data = User.query.filter_by(email=email).first()
+                # check if email exist in database
+                elif user_data:
+                    error_msg_dict[field] = "This email is already taken."
+                    
 
-            # if user_data.phone_number:
-            #     error_msg_dict[field] = "This phone number is already taken."
+            # validate phone field
+            if field == 'phone':
+                phone = value
+                numerals = "0123456789+"
 
-            if "+" not in phone:
-                error_msg_dict[field] = "Phone number is invalid."
+                if "+" not in phone:
+                    error_msg_dict[field] = "Phone number is invalid."
+
+                user_data = User.query.filter_by(phone_number=phone).first() # By phone number
+
+                if user_data:
+                    error_msg_dict[field] = "This phone number is already taken."
+
+                else:
+                    for index in range(len(phone)):
+                        if phone[index] not in numerals:
+                            error_msg_dict[field] = "Phone number is invalid."
+
+                        elif phone[0:3] != "+65":
+                            error_msg_dict[field] = "Phone number is invalid."
+
+                        elif len(phone) != 11:
+                            error_msg_dict[field] = "Phone number is invalid."
+
+            if error_msg_dict['password'] == '': # value of password field is not empty                                 
+                # validate passwords field
+                if field == 'password':
+                    password = value
+
+                    if len(password) < 8:
+                        error_msg_dict[field] = "Password must be at least 8 characters."
+
+                    else: # password field is valid
+                        password_confirm = req['password_confirm']
+
+                        if password_confirm != password:
+                            error_msg_dict['password_confirm'] = "Password must be the same."
 
             else:
-                for index in range(len(phone)):
-                    if phone[index] not in numerals:
-                        error_msg_dict[field] = "Phone number is invalid."
+                error_msg_dict['password_confirm'] = "Please enter your password first."
 
-                    elif phone[0:3] != "+65":
-                        error_msg_dict[field] = "Phone number is invalid."
+            # validate email field
+            if field == 'password_confirm':
+                password_confirm = value
 
-                    elif len(phone) != 11:
-                        error_msg_dict[field] = "Phone number is invalid."
+                if len(password_confirm) < 8:
+                    error_msg_dict[field] = "Password must be at least 8 characters."
 
-        # validate passwords field
-        if field == 'password':
-            password = value
-
-            if len(password) < 8:
-                error_msg_dict[field] = "Password must be at least 8 characters."
-
-        if field == 'password_confirm':
-            password_confirm = value
-
-            if password_confirm != password:
-                error_msg_dict[field] = "Password must be the same."
+        else:
+            if error_msg_dict['email'] == "Field is empty.":
+                error_msg_dict[field] = "Please enter your email first."
 
     return error_msg_dict
                     
