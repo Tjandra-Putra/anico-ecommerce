35a6532958ecde484517a9b4b3251e83dcf08b6d
Tjandra <tjandraputra2000@gmail.com>
2021-12-02 20:42:23 +0800

    update: footer title

--- front-end/src/components/user/Layout/Footer/Footer.js
+++ front-end/src/components/user/Layout/Footer/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
           <div className="col-md-8">
             <div className="title">
               Explore
-              <span style={{ fontFamily: "var(--font-poppins)" }}>anico.</span>
+              <span style={{ fontFamily: "var(--font-poppins)" }}> anico.</span>
             </div>
           </div>
           <div className="col-md-4 text-right">
