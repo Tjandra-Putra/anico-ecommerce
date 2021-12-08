9ecd085326c88a0fb36418fabaa24fe46c19112b
Tjandra <tjandraputra2000@gmail.com>
2021-12-08 22:00:44 +0800

    update: fixed nav collapse toggle hamburger animation

--- front-end/src/index.js
+++ front-end/src/index.js
@@ -35,16 +35,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 );
-
-// // ========================== Hamburger Menu Animation ==========================
-// // Look for .hamburger
-// var hamburger = document.querySelector(".hamburger");
-
-// // On click
-// if (hamburger) {
-//   hamburger.addEventListener("click", function () {
-//     // Toggle class "is-active"
-//     hamburger.classList.toggle("is-active");
-//     // Do something else, like open/close menu
-//   });
-// }
