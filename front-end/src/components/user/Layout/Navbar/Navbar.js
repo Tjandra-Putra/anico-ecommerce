3ec2105c75757dce07ebc0e4c58e54a62ea143de
Tjandra <tjandraputra2000@gmail.com>
2021-12-11 14:33:24 +0800

    update: Navbar Mobile View

--- front-end/src/components/user/Layout/Navbar/Navbar.js
+++ front-end/src/components/user/Layout/Navbar/Navbar.js
@@ -11,105 +11,192 @@ import favouriteIcon from "../../../../assets/img/heart.png";
 
 const Navbar = (props) => {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-white" id="navbar">
-      <div className="container">
-        <Link className="navbar-brand" to="/">
-          anico.
-        </Link>
-        <button
-          className="hamburger hamburger--collapse navbar-toggler"
-          type="button"
-          data-toggle="collapse"
-          data-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="hamburger-box">
-            <span className="hamburger-inner"></span>
-          </span>
-        </button>
+    <div className="Navbar">
+      {/* ======================= DESKTOP NAV ======================= */}
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-white d-none d-sm-block"
+        id="navbar"
+      >
+        <div className="container">
+          <Link className="navbar-brand" to="/">
+            anico.
+          </Link>
+          <button
+            className="hamburger hamburger--collapse navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarNav"
+            aria-controls="navbarNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
 
-        <div
-          className="collapse navbar-collapse"
-          id="navbarNav"
-          // data-toggle={isMobile ? "collapse" : ""}
-          // data-target={isMobile ? "#navbarNav" : ""}
-          data-toggle="collapse"
-          data-target="#navbarNav"
-        >
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <Link className="nav-link pr-4" to="/products">
-                Products
-              </Link>
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/support">
-                Support
-              </Link>
-            </li>
-          </ul>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link pr-4" to="/products">
+                  Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/support">
+                  Support
+                </Link>
+              </li>
+            </ul>
 
-          <ul className="navbar-nav ml-auto">
-            {props.sessionAuthData ? (
-              <Link className="nav-link" to="/cart">
-                <li className="nav-item mr-2" style={{ marginTop: "1.2rem" }}>
-                  welcome back &nbsp;
-                  {props.sessionAuthData.email.substring(
-                    0,
-                    props.sessionAuthData.email.lastIndexOf("@")
-                  )}
-                </li>
-              </Link>
-            ) : null}
+            <ul className="navbar-nav ml-auto">
+              {props.sessionAuthData ? (
+                <Link className="nav-link" to="/cart">
+                  <li className="nav-item mr-2" style={{ marginTop: "1.2rem" }}>
+                    welcome back &nbsp;
+                    {props.sessionAuthData.email.substring(
+                      0,
+                      props.sessionAuthData.email.lastIndexOf("@")
+                    )}
+                  </li>
+                </Link>
+              ) : null}
 
-            <li
-              className="nav-item  d-none d-sm-block"
-              style={{ marginTop: "1rem" }}
-            >
-              <Link className="nav-link" to="/favourite">
-                <img
-                  src={favouriteIcon}
-                  className="img-fluid"
-                  alt="shopping bag"
-                  width="25"
-                />
-              </Link>
-            </li>
-            {!props.sessionAuthData ? (
               <li
-                className="nav-item pl-1  d-none d-sm-block"
+                className="nav-item  d-none d-sm-block"
                 style={{ marginTop: "1rem" }}
               >
-                <Link className="nav-link" to="/login">
+                <Link className="nav-link" to="/favourite">
                   <img
-                    src={userIcon}
+                    src={favouriteIcon}
                     className="img-fluid"
-                    alt="userIcon"
+                    alt="shopping bag"
                     width="25"
                   />
                 </Link>
               </li>
-            ) : null}
-            <li
-              className="nav-item pl-1  d-none d-sm-block"
-              style={{ marginTop: "1rem" }}
-            >
-              <Link className="nav-link" to="/cart">
-                <img
-                  src={shoppingBagIcon}
-                  className="img-fluid d-inline"
-                  alt="shopping bag"
-                  width="25"
-                />
-                <div className="item-count">{props.itemCount}</div>
-              </Link>
-            </li>
-          </ul>
+              {!props.sessionAuthData ? (
+                <li
+                  className="nav-item pl-1  d-none d-sm-block"
+                  style={{ marginTop: "1rem" }}
+                >
+                  <Link className="nav-link" to="/login">
+                    <img
+                      src={userIcon}
+                      className="img-fluid"
+                      alt="userIcon"
+                      width="25"
+                    />
+                  </Link>
+                </li>
+              ) : null}
+              <li
+                className="nav-item pl-1  d-none d-sm-block"
+                style={{ marginTop: "1rem" }}
+              >
+                <Link className="nav-link" to="/cart">
+                  <img
+                    src={shoppingBagIcon}
+                    className="img-fluid d-inline"
+                    alt="shopping bag"
+                    width="25"
+                  />
+                  <div className="item-count">{props.itemCount}</div>
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+      {/* ======================= MOBILE NAV ======================= */}
+      <nav
+        className="navbar navbar-expand-lg navbar-light bg-white d-block d-sm-none"
+        id="navbar"
+      >
+        <div class="d-flex flex-row">
+          <div class="flex-item flex-hamburger">
+            <button
+              className="hamburger hamburger--collapse navbar-toggler"
+              type="button"
+              data-toggle="collapse"
+              data-target="#navbarNav"
+              aria-controls="navbarNav"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="hamburger-box">
+                <span className="hamburger-inner"></span>
+              </span>
+            </button>
+          </div>
+          <div class="flex-item flex-favourite">
+            <Link className="nav-link" to="/favourite">
+              <img src={favouriteIcon} alt="shopping bag" width="25" />
+            </Link>
+          </div>
+          <div class="flex-item"></div>
+          <div class="flex-item flex-brand">
+            <div class="nav-mobile-flex-item-brand">
+              <Link className="navbar-brand" to="/">
+                anico.
+              </Link>
+            </div>
+          </div>
+          <div class="flex-item"></div>
+          <div class="flex-item flex-user">
+            {!props.sessionAuthData ? (
+              <Link className="nav-link" to="/login">
+                <img src={userIcon} alt="userIcon" width="25" />
+              </Link>
+            ) : null}
+          </div>
+          <div class="flex-item flex-cart">
+            <Link className="nav-link" to="/cart">
+              <img src={shoppingBagIcon} alt="shopping bag" width="25" />
+              <div className="item-count">{props.itemCount}</div>
+            </Link>
+          </div>
+        </div>
+
+        <div className="container">
+          <div
+            className="collapse navbar-collapse"
+            id="navbarNav"
+            // data-toggle={isMobile ? "collapse" : ""}
+            // data-target={isMobile ? "#navbarNav" : ""}
+            data-toggle="collapse"
+            data-target="#navbarNav"
+          >
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link pr-4" to="/products">
+                  Products
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/support">
+                  Support
+                </Link>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              {props.sessionAuthData ? (
+                <Link className="nav-link" to="/cart">
+                  <li className="nav-item mr-2" style={{ marginTop: "1.2rem" }}>
+                    welcome back &nbsp;
+                    {props.sessionAuthData.email.substring(
+                      0,
+                      props.sessionAuthData.email.lastIndexOf("@")
+                    )}
+                  </li>
+                </Link>
+              ) : null}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
   );
 };
 
